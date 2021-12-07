8fde68b65e61a44405fac2eea814bcdeb61ae88b
Wioleta Lipinska <94996714+Wiolip@users.noreply.github.com>
2021-12-07 16:06:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//  ---- sticky menu ------
+
+window.addEventListener("scroll", function () {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0)
+})
+
+
+
+
